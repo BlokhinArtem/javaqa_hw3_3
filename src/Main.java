b0eb848e9b414549885bde018a597afb19a60621
Artem Blokhin <rtmdragee@gmail.com>
2024-03-22 02:22:52 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        CreditPaymentService service = new CreditPaymentService();
+        int amount = 1_000_000;
+        double percentPerYear = 9.99;
+        int year = 3;
+        double monthlyPayment = service.calculate(amount, percentPerYear,year);
+        int roundingNumber = (int) monthlyPayment; //преобразование месячного платежа в int
+        System.out.println("ежемесячный платёж" + roundingNumber);
+
+
+
+
+
+    }
+}
